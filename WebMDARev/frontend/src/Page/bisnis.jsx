68e42969910372edc28ab9cebb6d836b45e9bef8
unknown <fahrimuhammad38915@gmail.com>
2025-05-08 10:45:35 +0800

    commit

--- WebMDARev/frontend/src/Page/bisnis.jsx
+++ WebMDARev/frontend/src/Page/bisnis.jsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavbarProject from "../Component/navbarProject";
+import axios from "axios";
+import Footer from "./fotter";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 function Bisnis() {
+
+
+
+
+    const [bisnisList, setBisnisList] = useState([]);
+
+    useEffect(() => {
+        getBisnisData();
+        AOS.init({
+            duration: 1000,
+        });
+    }, []);
+
+    const getBisnisData = async () => {
+        try {
+            const res = await axios.get("http://127.0.0.1:8000/api/user/bisnis");
+            setBisnisList(res.data.bisnisUser);
+        } catch (error) {
+            alert('Erorr Pada Tampilan');
+        }
+    }
+
     return (
         <div>
             <NavbarProject />
             <section>
                 <div className="bg-bisnis d-flex align-items-center">
-                    <div className="container-fluid p-5">
+                    <div className="container-fluid p-5" data-aos="fade-right">
                         <div className="row">
                             <div className="col-md-5">
                                 <h1 className=" display-1 fw-bold" style={{ color: '#F16022' }}>Tambang Emas <span style={{ color: "#115258" }}>Pertama</span></h1>
@@ -21,6 +47,65 @@ function Bisnis() {
                         </div>
                     </div>
                 </div>
+            </section>
+            <section>
+                <div className="container-fluid p-5">
+                    <div className="row p-5">
+                        <div className="col-auto p-5" data-aos="fade-right">
+                            <img
+                                className="img-fluid"
+                                src="/Image/AwakMasCol.png"
+                                alt=""
+                                style={{ width: "300px", height: "auto" }}
+                            />
+                        </div>
+                        <div className="col-auto">
+                            {/* garis atas */}
+                            <div className="garis-aw-p"></div>
+                        </div>
+                        <div className="col" data-aos="fade-down">
+                            {
+                                bisnisList.length > 0 ? (
+                                    bisnisList.map((bisnis) => (
+                                        <p key={bisnis.uuid} className="fs-3">{bisnis.deskripsi_bisnis}</p>
+                                    ))
+                                ) : (
+                                    <div>
+                                        <h5>No Data</h5>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className="container mt-5 " data-aos="fade-down">
+                    <div className="row mt-4">
+                        {bisnisList.length > 0 ? (
+                            <div className="col p-5">
+                                {bisnisList.map((bisnis) => ((
+                                    <div key={bisnis.uuid} className="responsive-iframe-container">
+                                        <iframe
+                                            className="rounded-5 shadow-lg"
+                                            src={bisnis.link_video}
+                                            title="YouTube video player"
+                                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                            allowFullScreen
+                                        ></iframe>
+                                    </div>
+                                )))}
+                            </div>
+                        ) : (
+                            <div className="justify-content-center">
+                                <h5>No Data Yet</h5>
+                            </div>
+                        )};
+                    </div>
+                </div>
+            </section>
+            <section>
+                <Footer />
             </section>
         </div>
     )
